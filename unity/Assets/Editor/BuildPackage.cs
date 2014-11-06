5edcb88cf1f5371aae1548b576056ae5215e33fe
Tim Leader <tim@nerfgames.co.uk>
2014-11-06 23:30:25 +0000

    Build Script

---
+++ unity/Assets/Editor/BuildPackage.cs
@@ -0,0 +1,20 @@
+﻿
+using System.IO;
+
+using UnityEngine;
+using UnityEditor;
+
+public class BuildPackage 
+{
+	public static string[] m_PackageInputPaths = 
+	{
+		"Assets/Invocation"
+	};
+	public const string m_PackageOutputPath = "../build/invoker.unitypackage";
+
+	[MenuItem("Build/Invoker/Build Package")]
+	public static void BuildUnityPackage()
+	{
+		AssetDatabase.ExportPackage(m_PackageInputPaths, m_PackageOutputPath, ExportPackageOptions.Recurse);
+	}
+}
